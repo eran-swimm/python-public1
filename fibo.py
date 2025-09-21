48e98beb6d446a50a1bf354fefd4236a338607c8
Eran Keydar <eran@swimm.io>
2025-09-21 18:01:34 +0300

    Update fibo.py

--- fibo.py
+++ fibo.py
@@ -10,7 +10,7 @@ def fibo(n):
         a, b = 0, 1
         for _ in range(2, n):
             a, b = b, a + b
-        return b
+        return b+1
 
 if __name__ == "__main__":
     for i in range(1, 11):
